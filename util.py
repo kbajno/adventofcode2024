b9e3123232916fcfee2ed0df3f38d63fa8b3e0c4
Katherine Bajno <kbajno@gmail.com>
2024-12-02 19:30:16 -0800

    Day2 (#1)
    * add util for parsing input
    
    * satisfy part 1
    
    * satisfy part 2

---
+++ util.py
@@ -0,0 +1,8 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def get_input(day):
+    load_dotenv(".env")
+    r = requests.get(f"https://adventofcode.com/2024/day/{day}/input", headers={"Cookie": f"session={os.environ["session_cookie"]}"})
+    return r.text

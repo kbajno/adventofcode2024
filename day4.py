88a46ed1ec4721be97ca10bfa6081a952f9f30da
Katherine Bajno <kbajno@gmail.com>
2024-12-04 15:01:17 -0800

    get vertical forwards

--- day4.py
+++ day4.py
@@ -1,28 +1,56 @@
 from util import get_input
 import re
 
-    
+def find_vertical_matches(x):
+    print("vertical")
+    matches = 0
+    # forwards
+    print(f'max len: {len(x)}')
+    for i in range(len(x)):
+        for j in range(len(x)):
+            # If we found an X, check for MAS
+            if i+3 < len(x):
+                if x[i][j] == 'X':
+                    if x[i+1][j] == 'M':
+                        if x[i+2][j] == 'A':
+                            if x[i+3][j] == 'S':
+                                matches += 1
+            # else:
+            #     print(f'Skipping index {i} because i+3 is {i+3} and max len is {len(x)}')
+
+    # backwards
+    return matches
+ 
+def find_horizontal_matches(x):
+    print("horizontal")
+    # forwards
+
+    # backwards
+
+def find_diagonal_matches(x):
+    print("diagonal")   
+    # forwards
+
+    # backwards
 
 def main():
     # Part 1
     input = get_input("4")
+    word_search = str.splitlines(input)
 
-    # Word search conditions:
+    # for idx, line in enumerate(input.rstrip()):
+    #     print(input)
+    #     # split_line = line.rstrip().split()
+    #     # word_search_matrix.append([line.rstrip()])
+
+    print(word_search)
+
     # horizontal
+    print(find_vertical_matches(word_search))
     # vertical
     # diagonal
     # backwards
     # overlapping other words??
     # find ALL instances of XMAS
-    print(input)
-    word_search_matrix = []
-
-    for idx, line in enumerate(input.rstrip()):
-        split_line = line.rstrip().split()
-        word_search_matrix.append(split_line)
-
-    print(word_search_matrix)
-
-
 if __name__ == "__main__":
     main()

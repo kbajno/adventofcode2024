11f2b1479916b449847aa27964ba32193aa1776e
Katherine Bajno <kbajno@gmail.com>
2024-12-04 15:03:59 -0800

    get vertical forwards and backwards

--- day4.py
+++ day4.py
@@ -5,7 +5,7 @@ def find_vertical_matches(x):
     print("vertical")
     matches = 0
     # forwards
-    print(f'max len: {len(x)}')
+    # print(f'max len: {len(x)}')
     for i in range(len(x)):
         for j in range(len(x)):
             # If we found an X, check for MAS
@@ -20,15 +20,15 @@ def find_vertical_matches(x):
 
     # backwards
     for i in range(len(x)-1, -1, -1):
-        print(i)
-        # for j in range(len(x)):
-        #     # If we found an X, check for MAS
-        #     if i+3 < len(x):
-        #         if x[i][j] == 'X':
-        #             if x[i+1][j] == 'M':
-        #                 if x[i+2][j] == 'A':
-        #                     if x[i+3][j] == 'S':
-        #                         matches += 1
+        # print(i)
+        for j in range(len(x)):
+            # If we found an X, check for MAS
+            if i-3 < -1:
+                if x[i][j] == 'X':
+                    if x[i-1][j] == 'M':
+                        if x[i-2][j] == 'A':
+                            if x[i-3][j] == 'S':
+                                matches += 1
 
     return matches
  

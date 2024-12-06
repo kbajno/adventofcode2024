b595b3f6b05383b98e51ea69adc4cb5cb37e1be0
Katherine Bajno <kbajno@gmail.com>
2024-12-06 15:49:24 -0800

    finished p1

--- day6.py
+++ day6.py
@@ -35,7 +35,8 @@ def moveDown(map, guard_i, guard_j):
             # Turn 90 degrees at obstacle
             map[i-1][guard_j] = '<'
             return map, False
-        
+    
+    print('we should be done?')
     return map, True
 
 def moveLeft(map, guard_i, guard_j):
@@ -53,7 +54,7 @@ def moveLeft(map, guard_i, guard_j):
             map[guard_i][j+1] = '^'
             return map, False
         
-    # return map, True
+    return map, True
 
 def moveRight(map, guard_i, guard_j):
     map[guard_i][guard_j] = 'X'
@@ -75,46 +76,65 @@ def prettyPrint(map):
     print(np.matrix(map))    
 
 def main():
-    # input = get_input("6").strip()
-    input = """....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#..."""
+    input = get_input("6").strip()
+#     input = """....#.....
+# .........#
+# ..........
+# ..#.......
+# .......#..
+# ..........
+# .#..^.....
+# ........#.
+# #.........
+# ......#..."""
     map = str.splitlines(input)
     for idx, m in enumerate(map):
         map[idx] = list(m)
 
-    print('START')
+    done = False
+    while(done != True):
+        print('START')
+        prettyPrint(map)
+
+        print('MOVING UP')
+        i, j = locateGuard(map)
+        print(f'GUARD IS AT: {i}, {j}')
+        map, done = moveUp(map, i, j)
+        if done == True:
+            break
+        prettyPrint(map)
+
+        print('MOVING RIGHT')
+        i, j = locateGuard(map)
+        print(f'GUARD IS AT: {i}, {j}')
+        map, done = moveRight(map, i, j)
+        if done == True:
+            break
+        prettyPrint(map)
+
+        print('MOVING DOWN')
+        i, j = locateGuard(map)
+        print(f'GUARD IS AT: {i}, {j}')
+        map, done = moveDown(map, i, j)
+        if done == True:
+            break
+        prettyPrint(map)
+
+        print('MOVING LEFT')
+        i, j = locateGuard(map)
+        print(f'GUARD IS AT: {i}, {j}')
+        map, done = moveLeft(map, i, j)
+        if done == True:
+            break
+        prettyPrint(map)
+
+    print('ALL DONE')
     prettyPrint(map)
 
-    print('MOVING UP')
-    i, j = locateGuard(map)
-    print(f'GUARD IS AT: {i}, {j}')
-    map, done = moveUp(map, i, j)
-    prettyPrint(map)
-
-    print('MOVING RIGHT')
-    i, j = locateGuard(map)
-    print(f'GUARD IS AT: {i}, {j}')
-    map, done = moveRight(map, i, j)
-    prettyPrint(map)
-
-    print('MOVING DOWN')
-    i, j = locateGuard(map)
-    print(f'GUARD IS AT: {i}, {j}')
-    map, done = moveDown(map, i, j)
-    prettyPrint(map)
-
-    print('MOVING LEFT')
-    i, j = locateGuard(map)
-    print(f'GUARD IS AT: {i}, {j}')
-    map, done = moveLeft(map, i, j)
-    prettyPrint(map)
+    total = 0
+    for m in map:
+        total += m.count('X')
+  
+    print(f'total: {total}')
 if __name__ == "__main__":
     main()

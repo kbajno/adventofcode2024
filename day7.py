268a52aae6d7dea1c480807fe379e0a46ff22142
Katherine Bajno <kbajno@gmail.com>
2024-12-07 12:23:59 -0800

    refactor

--- day7.py
+++ day7.py
@@ -11,6 +11,32 @@ def multAll(nums):
 def compare(num1, num2):
     return int(num1) == int(num2)
 
+def evalOps(nums, op):
+    cmd = [nums[0]]
+    for idx, o in enumerate(op):
+        cmd.append(o)
+        cmd.append(nums[idx+1])
+
+    print(f'cmd to eval: {cmd}')
+    return eval(''.join(cmd))
+
+def constructNumsObj(input):
+    nums = {}
+    lines = input.splitlines()
+    for line in lines:
+        l = line.split(':')
+        total = l[0]
+        n = l[1].strip().split(' ')
+        nums[total] = n
+    
+    return nums
+
+def getResult(true_vals):
+    total = 0
+    for val in true_vals:
+        total += int(val)
+
+    return total
 def main():
     # input = get_input("7")
     input = """190: 10 19
@@ -23,40 +49,21 @@ def main():
 21037: 9 7 18 13
 292: 11 6 16 20"""
 
-    nums = {}
-    lines = input.splitlines()
-    for line in lines:
-        l = line.split(':')
-        total = l[0]
-        n = l[1].strip().split(' ')
-        nums[total] = n
-
-    operands = ['+', '*']
+    nums = constructNumsObj(input)
     true_vals = []
 
     for i in nums:
-        print(f'Evaluating {i}: {nums[i]}')
-        # try all +
+        total_combinations = 2**(len(nums[i])-1)
+        op_spots = len(nums[i])-1
+        print(f'Evaluating {i}: {nums[i]} - total_combinations: {total_combinations}')
+
+        ops = ['+'] * op_spots
         test_val = i
-        sum = addAll(nums[i])
-        print(f'test_val: {test_val} sum: {sum}')
-
+        sum = evalOps(nums[i], ops)
+        print(f'test_val: {test_val} sum: {sum}')      
         if compare(test_val,  sum):
-            true_vals.append(test_val)
+            true_vals.append(test_val)       
 
-        # try all *
-        test_val = i
-        sum = multAll(nums[i])
-        print(f'test_val: {test_val} sum: {sum}')
-
-        if compare(test_val,  sum):
-            true_vals.append(test_val)
-
-        # total_combinations = ((len(nums[n])-1) * (len(operands)))
-
-    total = 0
-    for val in true_vals:
-        total += int(val)
-    print(f'total calibration result: {total}')
+    print(f'total calibration result: {getResult(true_vals)}')
 if __name__ == "__main__":
     main()

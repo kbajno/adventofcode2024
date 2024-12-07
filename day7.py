2d2bbb3d09afe90144f73fafd57f0a88faa50d60
Katherine Bajno <kbajno@gmail.com>
2024-12-07 12:05:35 -0800

    do all add and mult conditions

--- day7.py
+++ day7.py
@@ -1,5 +1,16 @@
 from util import get_input
 
+def addAll(nums):
+    op = ('+').join(nums)
+    return eval(op)
+
+def multAll(nums):
+    op = ('*').join(nums)
+    return eval(op)
+
+def compare(num1, num2):
+    return int(num1) == int(num2)
+
 def main():
     # input = get_input("7")
     input = """190: 10 19
@@ -19,8 +30,33 @@ def main():
         total = l[0]
         n = l[1].strip().split(' ')
         nums[total] = n
-    print(nums)
 
+    operands = ['+', '*']
+    true_vals = []
 
+    for i in nums:
+        print(f'Evaluating {i}: {nums[i]}')
+        # try all +
+        test_val = i
+        sum = addAll(nums[i])
+        print(f'test_val: {test_val} sum: {sum}')
+
+        if compare(test_val,  sum):
+            true_vals.append(test_val)
+
+        # try all *
+        test_val = i
+        sum = multAll(nums[i])
+        print(f'test_val: {test_val} sum: {sum}')
+
+        if compare(test_val,  sum):
+            true_vals.append(test_val)
+
+        # total_combinations = ((len(nums[n])-1) * (len(operands)))
+
+    total = 0
+    for val in true_vals:
+        total += int(val)
+    print(f'total calibration result: {total}')
 if __name__ == "__main__":
     main()
